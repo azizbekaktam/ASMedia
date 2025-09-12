d78eea51ec84534708f02813cee65a1d5e01a251
Azizbek Aktamov <aktamovazizbek563@gmail.com>
2025-09-12 16:46:55 +0500

    usersPage

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { Menu, X } from "lucide-react";
 import Search from "./Search";
 import LogOut from "./LogOut";
+import UsersPage from "./UserPage";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,14 +13,11 @@ export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-gray-900 shadow-md">
       <div className="max-w-7xl mx-auto px-6 py-3 flex items-center justify-between">
-        
         <div className="font-extrabold text-2xl tracking-wide text-yellow-500">
           As<span className="text-white">Media</span>
         </div>
 
         <ul className="hidden md:flex gap-8 font-medium text-white">
-
-
           <Link href="/Movies" className="relative group">
             <li className="cursor-pointer hover:text-yellow-500 transition-colors">
               Home
@@ -33,7 +31,7 @@ export default function Navbar() {
             </li>
             <span className="absolute left-0 -bottom-1 w-0 group-hover:w-full h-0.5 bg-yellow-500 transition-all"></span>
           </Link>
-                    <Link href="/LIkedPage" className="relative group">
+          <Link href="/LIkedPage" className="relative group">
             <li className="cursor-pointer hover:text-yellow-500 transition-colors">
               Like
             </li>
@@ -45,7 +43,9 @@ export default function Navbar() {
           <div className="hidden md:block w-40 lg:w-56 right-10px">
             <Search />
           </div>
-
+          <div>
+            <UsersPage />
+          </div>
           <Link
             href={"/LIkedPage"}
             className="w-9 h-9 rounded-full bg-gradient-to-r from-yellow-400 to-yellow-500 flex items-center justify-center text-black font-bold text-xs shadow-md"
@@ -69,9 +69,15 @@ export default function Navbar() {
           <div className="w-full">
             <Search />
           </div>
-          <Link href="/" className="block hover:text-yellow-500">Home</Link>
-          <Link href="/Movies" className="block hover:text-yellow-500">Movies</Link>
-          <Link href="/Cartoon" className="block hover:text-yellow-500">Cinema</Link>
+          <Link href="/" className="block hover:text-yellow-500">
+            Home
+          </Link>
+          <Link href="/Movies" className="block hover:text-yellow-500">
+            Movies
+          </Link>
+          <Link href="/Cartoon" className="block hover:text-yellow-500">
+            Cinema
+          </Link>
         </div>
       )}
     </nav>
